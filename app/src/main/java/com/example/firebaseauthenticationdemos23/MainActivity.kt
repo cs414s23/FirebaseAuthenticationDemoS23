20ba4c73cdf6e64b71fee3cad6d9dc08c237b6d9
CS416F19 <cs416f19@gmail.com>
2023-04-24 12:58:57 -0400

    Initial commit

---
+++ app/src/main/java/com/example/firebaseauthenticationdemos23/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.firebaseauthenticationdemos23
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // #### Authentication using FirebaseAuth #####
+
+        // Get instance of the FirebaseAuth
+        val currentUser = FirebaseAuth.getInstance().currentUser
+
+        // If currentUser is null, open the RegisterActivity
+        if (currentUser == null) {
+            startRegisterActivity()
+        } else {
+            findViewById<TextView>(R.id.person_name).text = currentUser.displayName
+            findViewById<TextView>(R.id.person_email).text = currentUser.email
+            Glide.with(this)
+                .load(currentUser.photoUrl)
+                .placeholder(R.drawable.baseline_person_24)
+                .circleCrop()
+                .into(findViewById<ImageView>(R.id.person_image))
+        }
+    }
+
+    // An helper function to start our RegisterActivity
+    private fun startRegisterActivity() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+        // Make sure to call finish(), otherwise the user would be able to go back to the MainActivity
+        finish()
+    }
+
+
+    // This override function is used to create menu option where you can see on the top right corner
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_options, menu)
+        return true
+    }
+
+    // This override function is used to handle if menu_option (logout) is selected.
+    // If so, the user will be signed out.
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                // User chose the "logout" item, logout the user then
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
+
+                AuthUI.getInstance().signOut(this)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            // After logout, start the RegisterActivity again
+                            startRegisterActivity()
+                        }
+                        else {
+                            Log.e(TAG, "Task is not successful:${task.exception}")
+                        }
+                    }
+                true
+            }
+            else -> {
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+}
